49e603d92ffa42d3526b166b59c4b90a158a9754
Travis South <toto.hehe@gmail.com>
2012-11-12 20:39:44 +0800

    Initial layout

---
+++ WebReservations/WebReservations/Lib/Helper.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * @Author: jcapagcuan 
+ * @Package: WebReservations.Lib
+ * Helper class for hotel web reservations. All custom functions related to site is located here
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using System.Web.Security;
+using System.Web.Profile;
+using System.Configuration.Provider;
+using WebReservations.Controllers;
+
+namespace WebReservations.Lib
+{
+    public class Helper
+    {
+        
+    }
+}
---
+++ WebReservations/WebReservations/Scripts/site.js
@@ -0,0 +1,15 @@
+﻿/*
+* @Author: jcapagcuan
+* Custom Javascript file.
+*/
+if (typeof window.console == "undefined") {
+    window.console = {
+        log: function (msg) {
+            return;
+        }
+    };
+}
+
+jQuery(document).ready(function ($) {
+    $('.body').fadeIn(500);
+});
