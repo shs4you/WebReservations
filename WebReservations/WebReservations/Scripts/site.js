1cfcb0faa2cc1d67b2033e1d8eecb299f366ec38
Travis South <toto.hehe@gmail.com>
2012-11-14 06:31:01 +0800

    Updated select dates page

--- WebReservations/WebReservations/Scripts/site.js
+++ WebReservations/WebReservations/Scripts/site.js
@@ -2,7 +2,7 @@
 * @Author: jcapagcuan
 * Custom Javascript file.
 */
-if (typeof window.console == "undefined") {
+if (typeof window.console == 'undefined') {
     window.console = {
         log: function (msg) {
             return;
@@ -12,6 +12,8 @@ if (typeof window.console == "undefined") {
 
 var reservation_status;
 var tabs = ['dates', 'room_rates', 'enhance_stay', 'guest_details', 'confirm_booking'];
+var min_date = 1;
+var min_date_departure = 1;
 
 jQuery(document).ready(function ($) {
     $('.body').fadeIn(500);
@@ -19,13 +21,37 @@ jQuery(document).ready(function ($) {
     //Assigning of saved data from cookie
     reservation_data = $.cookie('reservation_data') ? $.cookie('reservation_data') : false;
     reservation_status = $.cookie('reservation_status') ? $.cookie('reservation_status') : false;
-    reservation_status = { "dates": true };
+    reservation_status = { 'dates': true };
+
+    $('#reservation_modal').modal({
+        keyboard: false,
+        backdrop: 'static',
+        show: false
+    });
+
+    $('#datepicker_arrival').datepicker({
+        showOn: 'button',
+        minDate: min_date,
+        onSelect: function(dateText, inst){
+            min_date_departure = new Date(dateText);
+            $( '#datepicker_departure' ).datepicker( 'option', 'minDate', min_date_departure.addDays(1).asString());
+        }
+    });
+
+    $('#datepicker_departure').datepicker({
+        showOn: 'button',
+        minDate: min_date_departure
+    });
+
+    $('#launch_modal').click(function () {
+        
+    });
 
     //Add checking befor proceeding with next step.
     $('a[data-toggle="tab"]').on('show', function (e) {
-        var target = $(e.target) // activated tab
-        var related = $(e.relatedTarget) // previous tab
-        var target_href = target.attr('href').replace("#", "");
+        var target = $(e.target); // activated tab
+        var related = $(e.relatedTarget); // previous tab
+        var target_href = target.attr('href').replace('#', '');
         if (!check_status(target_href)) {
             //Show alert
             $('.alert').show();
@@ -35,14 +61,39 @@ jQuery(document).ready(function ($) {
         else {
             return true;
         }
-    })
+    });
+
+    //Add checking befor proceeding to the clicked step.
+    /*
+    $('a[data-toggle="tab"]').click(function (e) {
+    var target_href = $(e.target).attr('href').replace('#', '');
+    console.log(target_href);
+    if(target_href == get_active_tab()){
+    return true;
+    }
+    if (!check_status(target_href)) {
+    //Show alert
+    $('.alert').show();
+    $('.alert').fadeOut(1500);
+    return false;
+    }
+    else if(get_active_tab() != 'confirm_booking') {
+    $.reservation(get_active_tab());
+    return true;
+    }
+    else{
+    return true;
+    }
+    });
+    */
 
     //Logic for clicking the next button
     $('#next').click(function () {
         var active_tab_id = get_active_tab();
         var next_tab = tabs.indexOf(active_tab_id) + 1;
+        $('#reservation_modal').modal('show');
         $.reservation(active_tab_id);
-        $('#steps li:eq(' + next_tab + ') a').trigger('click');
+        $('#steps li:eq(' + next_tab + ') a').tab('show');
     });
 
     //Logic for clicking the back button
@@ -50,7 +101,7 @@ jQuery(document).ready(function ($) {
         var active_tab_id = get_active_tab();
         var prev_tab = tabs.indexOf(active_tab_id) - 1;
         if (prev_tab < 0) return;
-        $('#steps li:eq(' + prev_tab + ') a').trigger('click');
+        $('#steps li:eq(' + prev_tab + ') a').tab('show');
     });
 });
 
@@ -81,30 +132,52 @@ function get_active_tab() {
     return $('#steps li.active a').attr('href').replace('#', '');
 }
 
+
+/*
+* Handler for the functions/logic/validations per step
+* @params options mixed
+* @return object
+*/
 (function ($) {
     var methods = {
         init: function (options) {
+            return this;
+        },
 
+        hide_modal: function(){
+            $('#reservation_modal').modal('hide');
         },
 
         dates: function (options) {
             reservation_status.room_rates = true;
+            methods.hide_modal();
+            return this;
         },
 
         room_rates: function (options) {
             reservation_status.enhance_stay = true;
+            reservation_status.guest_details = true;
+            reservation_status.confirm_booking = true;
+            methods.hide_modal();
+            return this;
         },
 
         enhance_stay: function (options) {
             reservation_status.guest_details = true;
+            methods.hide_modal();
+            return this;
         },
 
         guest_details: function (options) {
             reservation_status.confirm_booking = true;
+            methods.hide_modal();
+            return this;
         },
 
         confirm_booking: function (options) {
             reservation_status.confirm_booking = true;
+            methods.hide_modal();
+            return this;
         }
 
     };
@@ -116,6 +189,7 @@ function get_active_tab() {
         } else if (typeof method === 'object' || !method) {
             return methods.init.apply(this, arguments);
         } else {
+            console.log('Method ' + method + ' does not exist');
             $.error('Method ' + method + ' does not exist');
         }
 
