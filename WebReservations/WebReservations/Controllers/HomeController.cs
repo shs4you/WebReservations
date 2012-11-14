cce61ec71e3fc5c8e5882dfb9880e15b35c1b026
Travis South <toto.hehe@gmail.com>
2012-11-15 06:19:12 +0800

    Added availability service from OWS

--- WebReservations/WebReservations/Controllers/HomeController.cs
+++ WebReservations/WebReservations/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Diagnostics;
 using WebReservations.Lib;
+using WebReservations.Availability;
 
 namespace WebReservations.Controllers
 {
@@ -16,6 +17,82 @@ namespace WebReservations.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Test()
+        {
+            AvailabilityRequest ws = new AvailabilityRequest();
+            RegionalAvailabilityExtRequest reg = new RegionalAvailabilityExtRequest();
+            AvailabilityServiceSoapClient cli = new AvailabilityServiceSoapClient();
+            OGHeader og = new OGHeader();
+            EndPoint ep = new EndPoint();
+            EndPoint dest = new EndPoint();
+            AvailRequestSegment segment = new AvailRequestSegment();
+            HotelSearchCriterion hotel_search = new HotelSearchCriterion();
+            HotelReference hotel_ref = new HotelReference();
+            RoomStayCandidate room_stay = new RoomStayCandidate();
+            AvailabilityRequest request = new AvailabilityRequest();
+            Availability.TimeSpan time_span = new Availability.TimeSpan();
+            MinMaxRate rate = new MinMaxRate();
+            AvailabilityResponse response = new AvailabilityResponse();
+
+
+            ep.entityID = "OWS";
+            ep.systemType = "WEB";
+            dest.entityID = "1T";
+            dest.systemType = "PMS";
+
+            Random random = new Random();
+            int transId = random.Next(100, 999999);
+            og.Origin = ep;
+            og.Destination = dest;
+            og.transactionID = transId.ToString("000000");
+            og.timeStamp = DateTime.Now;
+            og.timeStampSpecified = true;
+
+            request.summaryOnly = true;
+
+            time_span.StartDate = DateTime.Now;
+            time_span.Item = DateTime.Now.AddDays(1);
+            
+            rate.currencyCode = "PHP";
+            rate.minimumRateSpecified = false;
+            rate.maximumRateSpecified = false;
+
+            segment.numberOfRooms = 1;
+            segment.numberOfRoomsSpecified = true;
+            segment.totalNumberOfGuests = 1;
+            segment.totalNumberOfGuestsSpecified = true;
+            segment.numberOfChildrenSpecified = false;
+            segment.availReqType = AvailRequestType.Room;
+
+
+            hotel_ref.chainCode = "CHA";
+            hotel_ref.hotelCode = "WCCH";
+            hotel_search.HotelRef = hotel_ref;
+
+            room_stay.invBlockCode = null;
+
+            segment.HotelSearchCriteria = new HotelSearchCriterion[1];
+            segment.RoomStayCandidates = new RoomStayCandidate[1];
+            request.AvailRequestSegment = new AvailRequestSegment[1];
+
+            segment.HotelSearchCriteria[0] = hotel_search;
+            segment.RoomStayCandidates[0] = room_stay;
+            segment.RateRange = rate;
+            segment.StayDateRange = time_span;
+            request.AvailRequestSegment[0] = segment;
+
+            response = cli.Availability(ref og, request);
+
+
+            /*
+            og.Intermediaries = 
+            ws.
+
+            cli.Availability(og, );
+             * */
+            return Json(response, JsonRequestBehavior.AllowGet);
         }
 
         //
